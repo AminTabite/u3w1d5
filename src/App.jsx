f4419e6a00e8b5f185ed7b13887ce92045a56366
AminTabite <Aminepicode13@gmail.com>
2025-07-25 11:17:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../components/Header";
+
+function App() {
+  return (
+    <>
+      <h1>Ciao</h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
