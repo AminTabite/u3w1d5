036a325cc3478a677d072adb14c6c456ea86560e
AminTabite <Aminepicode13@gmail.com>
2025-07-31 09:43:21 +0200

    some changes

--- components/Gallery1.jsx
+++ components/Gallery1.jsx
@@ -1,18 +1,17 @@
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Link, useNavigate } from "react-router-dom";
 
 const endpoint1 =
   "https://www.omdbapi.com/?i=tt3896198&apikey=7e50598d&s=supernatural";
+const Gallery1 = () => {
+  const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
 
-class Gallery1 extends Component {
-  state = {
-    movies: [],
-  };
-
-  loadMovies = () => {
+  const loadMovies = () => {
     fetch(endpoint1)
       .then((response) => {
         if (response.ok) {
@@ -24,46 +23,48 @@ class Gallery1 extends Component {
       .then((datas) => {
         console.log("ecco i risultati dei film");
         console.log(datas);
-        this.setState({
-          movies: datas.Search,
-        });
+        setMovies(datas.Search);
       })
       .catch((err) => {
         console.log("errore caricamento", err);
       });
   };
 
-  componentDidMount() {
-    this.loadMovies();
-  }
+  useEffect(() => {
+    loadMovies();
+  }, []);
 
-  render() {
-    return (
-      <div className="gallery-bg">
-        <Row className="justify-content-center mt-3">
-          {this.state.movies.map((movie) => (
-            <Col className="h-100 g-5" xs={12} md={8} lg={6} key={movie.imdbID}>
-              <Card className="Sizecard h-100 d-flex flex-column">
-                <Card.Img
-                  variant="top"
-                  className="img-fixed"
-                  src={movie.Poster}
-                />
-                <Card.Body className="flex-grow-1 d-flex flex-column justify-content-between">
-                  <div className="">
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Year}</Card.Text>
-                    <Card.Text>{movie.Type}</Card.Text>
-                  </div>
-                  <Button variant="primary">Start Movie</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="gallery-bg">
+      <Row className="justify-content-center mt-3">
+        {movies.map((movie) => (
+          <Col className="h-100 g-5" xs={12} md={8} lg={6} key={movie.imdbID}>
+            <Card className="Sizecard h-100 d-flex flex-column">
+              <Card.Img
+                variant="top"
+                className="img-fixed"
+                src={movie.Poster}
+              />
+              <Card.Body className="flex-grow-1 d-flex flex-column justify-content-between">
+                <div className="">
+                  <Card.Title>{movie.Title}</Card.Title>
+                  <Card.Text>{movie.Year}</Card.Text>
+                  <Card.Text>{movie.Type}</Card.Text>
+                </div>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    navigate("/movieDetails/" + movie.imdbID);
+                  }}>
+                  Details{" "}
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
 
 export default Gallery1;
--- components/MovieDetails.jsx
+++ components/MovieDetails.jsx
@@ -1,9 +1,98 @@
-const movieDetails = () => {
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const MovieDetails = () => {
+  const { moviecode } = useParams();
+  const navigate = useNavigate();
+  const [movies, setMovies] = useState({});
+  const [comment, setComment] = useState([]);
+
+  const endpointid = `https://www.omdbapi.com/?i=${moviecode}&apikey=e0cf1553`;
+  const endpointcomment = `https://striveschool-api.herokuapp.com/api/comments/${moviecode}`;
+
+  const getId = () => {
+    fetch(endpointid)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("caricamento id fallito");
+        }
+      })
+      .then((data) => {
+        console.log("ecco l'id del film");
+        console.log(data);
+        setMovies(data);
+      })
+      .catch((err) => {
+        console.log("errore caricamento", err);
+      });
+  };
+
+  const getComments = () => {
+    fetch(endpointcomment, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODc4YWRkNTYzZDk3MTAwMTUwZGY2MWQiLCJpYXQiOjE3NTI3MzkyODUsImV4cCI6MTc1Mzk0ODg4NX0.ksNBhJG5JWfSXzMJhRS54qyWSpL1i7vr38VYsTY0AT8",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("caricamento commenti fallito");
+        }
+      })
+      .then((data) => {
+        console.log("commenti?");
+        console.log(data);
+        setComment(data);
+      })
+      .catch((err) => {
+        console.log("errore caricamento commenti", err);
+      });
+  };
+
+  useEffect(() => {
+    getId();
+    getComments();
+  }, [moviecode]);
+
   return (
-    <div>
-      <p>ciao</p>
-    </div>
+    <Container>
+      <Row className="justify-content-center m-5">
+        <Col xs={12} md={6} className="text-center">
+          <Card>
+            <Card.Img variant="top" src={movies.Poster} />
+            <Card.Body>
+              <Card.Title>{movies.Title}</Card.Title>
+              <Card.Text>{movies.Year}</Card.Text>
+              <Card.Text>{movies.Type}</Card.Text>
+
+              <h5 className="mt-4">Commenti</h5>
+              {comment.length > 0 ? (
+                comment.map((com, index) => (
+                  <Card.Text key={index}>• {com.comment}</Card.Text>
+                ))
+              ) : (
+                <Card.Text>Nessun commento disponibile.</Card.Text>
+              )}
+
+              <Button
+                variant="primary"
+                className="mt-3"
+                onClick={() => {
+                  navigate("/");
+                }}>
+                Torna in Home
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default movieDetails;
+export default MovieDetails;
